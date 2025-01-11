99b8acdb838204a326a973080dcbd49ca3cd28b3
ty-abbott <tkabbott97@gmail.com>
2025-01-10 19:05:46 -0700

    continuing collector work, doing websockets

--- src/collector.py
+++ src/collector.py
@@ -2,20 +2,31 @@ import threading
 import time 
 from scapy.all import *
 import sys
+import websockets
+import asyncio 
 
-def collect(interface1):
+loop_bool = True
+
+async def collect(interface1):
+    async with websockets.connect("URL") as websocket:
+        recv_task = asyncio.create_task(recieve_stop(websocket))
     while(True):
         #create websocket connection
         #error handling with that 
         #start gathering from the interfaces 
         #figure out logic to only do new IPs 
         #send in websocket call to the server via the websocket
-
+        if loop_bool == False:
+            #stop the presses
 #REMEMBER WE CAN GIVE A LIST OF INTERFACES
-
+async def recieve_stop(websocket):
+    while True:
+        message = await websocket.recv()
+        if message == "Stop":
+            loop_bool = False
 def main():
     interface1 ="en1"
-    collect(interface1)
+    async.run(collect(interface1))
 
 
 
--- src/main.py
+++ src/main.py
@@ -4,6 +4,50 @@ from parameters import parameters
 from data import data
 from fastapi import FastAPI, WebSocket
 
+
+
+app = FastAPI()
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+        
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.get("/")
+def read_root():
+    return("hello": "world")
+#we are changing up the design. This will be easier, scalable, better
+@app.get("/addInterface") 
+def add_interface():
+        
+@app.WebSocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.recieve_text()
+            #do checks and store data 
+            #have a check here to see if we should keep recieving or if we need to do a stop for state reasons(report, whatever else)
+            if stop == True:
+                manager.broadcast("Stop") #the magic word 
+                #either on the client side have a timed stop or shut down the endpoint??
+    except WebSocketDisconnect:
+        #log that it had been disconnect
+        #wait for reconnect? 
+
+
 def main():
     '''
     load state
@@ -13,7 +57,6 @@ def main():
      - get the list of IPs that are currently in the buffer for analysis 
       
     '''
-    app = FastAPI()
 
 
     #get the adapters from database and if there are some returned then we can skip the getting of the paramters via function 
@@ -26,15 +69,6 @@ def main():
         paramObject.getParams()
         adapterList = paramObject.adapterList
     #when and where to save the interfaces to the database? 
-
-    @app.get("/")
-    def read_root():
-        return("hello": "world")
-#we are changing up the design. This will be easier, scalable, better
-    @app.get("/addInterface") 
-    def add_interface():
-        
-
 
 #the above will essentially just return an object of data that we will need to get this party started 
 # we can create this loop to just be checking for input, eventually there will be a systemd service that is running
