284e23bf468f57c572bfb7de60244bd48c3c139c
ty-abbott <tkabbott97@gmail.com>
2025-01-15 23:30:30 -0700

    keep trucking on the packet collector

--- src/collector.py
+++ src/collector.py
@@ -5,25 +5,31 @@ import sys
 import websockets
 import asyncio 
 
-loop_bool = True
+loop_bool = False
 
+def is_stop():#I suspect we may need to include a packet argument
+    return loop_bool
+
+def process_packet(pkt):
+    #ok here is where we build out the sniffing logic
+    #right now we will gather IP, dst Port, and Protocol 
+    
 async def collect(interface1):
     async with websockets.connect("URL") as websocket:
         recv_task = asyncio.create_task(recieve_stop(websocket))
-    while(True):
+        sniff(iface=interface1, stop_filter=is_stop, prn=process_packet)
+
         #create websocket connection
         #error handling with that 
         #start gathering from the interfaces 
         #figure out logic to only do new IPs 
         #send in websocket call to the server via the websocket
-        if loop_bool == False:
-            #stop the presses
 #REMEMBER WE CAN GIVE A LIST OF INTERFACES
 async def recieve_stop(websocket):
     while True:
         message = await websocket.recv()
         if message == "Stop":
-            loop_bool = False
+            loop_bool = True
 def main():
     interface1 ="en1"
     async.run(collect(interface1))
